b13c7b047cd1ce15b1a96eafcf560c520d474ed4
Popal Daudzai <pdaudzai@my.bcit.ca>
2025-10-04 21:10:05 -0700

    Added style changes

--- rate-my-landlord/static/js/animation.js
+++
@@ -1,6 +0,0 @@
-const registerBtn = document.querySelector("nav button");
-const container = document.querySelector(".container");
-
-registerBtn.addEventListener("click", () => {
-    container.classList.add("animate");
-});
