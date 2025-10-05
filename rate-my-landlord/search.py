e9597bffbead57231856893bc0fa36c1e59f779e
Voya Thomison <harveyxthomison@egmail.com>
2025-10-05 11:45:31 -0700

    fixed search

--- rate-my-landlord/search.py
+++ rate-my-landlord/search.py
@@ -1,47 +1,33 @@
 import firebase_admin
-from firebase_admin import firestore
-from flask import Blueprint, flash, redirect, render_template, request, session, url_for
+from firebase_admin import firestore, credentials
+import os
+import uuid
+
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
 
 blueprint = Blueprint('search', __name__, url_prefix='/search')
 db = firestore.client()
 
-
 @blueprint.route('/', methods=('GET', 'POST'))
 def search():
-    query_term = request.args.get('query') or request.form.get('query', '')
+    if request.method == 'POST':
+        redirect_url = url_for('search.search_results', query=request.form['query'], search=request.args.get("search"))
+        return redirect(redirect_url)
+    return render_template('search.html', label="landlords" if request.args.get("search") == "l" else "properties", offset=0)
 
-    if not query_term:
-        # No query entered, just render empty search page
-        return render_template(
-            'search.html',
-            label="landlords" if request.args.get(
-                "search") == "l" else "properties",
-            data=[],
-            offset=0
-        )
-
-    # Determine if searching landlords or properties
-    search_type = request.args.get("search", "l")  # default to landlords
-
-    if search_type == "l":
-        # Firestore query for landlord name
-        query = db.collection("landlord").where(
-            "name", ">=", query_term).where("name", "<=", query_term + "\uf8ff")
+@blueprint.route('/<query>', methods=('GET', 'POST'))
+def search_results(query):
+    if request.method == 'POST':
+        redirect_url = url_for('search.search_results', query=request.form['query'], search=request.args.get("search"))
+        return redirect(redirect_url)
+    if request.args.get("search") == "l":
+        query_data = db.collection("landlord").where("name", ">=", query).where("name", "<", query + "\uf8ff")
     else:
-        # Firestore query for property address
-        query = db.collection("property").where(
-            "addressLine1", ">=", query_term).where("addressLine1", "<=", query_term + "\uf8ff")
+        query_data = db.collection("property").where("addressLine1", ">=", query).where("addressLine1", "<", query + "\uf8ff")
+    data = query_data.get()
 
-    data = query.get()
-
-    # If exactly one landlord is found, redirect to their page
-    if search_type == "l" and len(data) == 1:
-        landlord_id = data[0].id
-        return redirect(url_for('landlord.landlord', landlord_id=landlord_id))
-
-    return render_template(
-        'search.html',
-        label="landlords" if search_type == "l" else "properties",
-        data=data,
-        offset=0
-    )
+    return render_template('search.html', label="landlords" if request.args.get("search") == "l" else "properties", data=data, offset=0)
