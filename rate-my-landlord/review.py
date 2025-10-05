e420a4703dc621fc4f1e57a922edd04e419935c1
Popal Daudzai <pdaudzai@my.bcit.ca>
2025-10-05 10:43:01 -0700

    Added design changes to review files

--- rate-my-landlord/review.py
+++ rate-my-landlord/review.py
@@ -12,6 +12,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 blueprint = Blueprint('review', __name__, url_prefix='/review')
 db = firestore.client()
 
+
 @blueprint.route('/submit/<landlord_id>/<property_id>', methods=('GET', 'POST'))
 def submit_review(landlord_id, property_id):
     if "user_id" not in session:
@@ -19,8 +20,10 @@ def submit_review(landlord_id, property_id):
     if request.method == 'POST':
         rating = request.form['rating']
         body = request.form['body']
-        landlord_id = db._get_collection_reference('landlord').document(landlord_id)
-        property_id = db._get_collection_reference('property').document(property_id)
+        landlord_id = db._get_collection_reference(
+            'landlord').document(landlord_id)
+        property_id = db._get_collection_reference(
+            'property').document(property_id)
         title = request.form['title']
         db.collection('review').add({
             'title': title,
